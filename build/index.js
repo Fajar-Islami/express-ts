320fa3ad2034a3b915d82022844433cf284522fc
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-10-31 22:15:07 +0700

    video 2 - api dengan expresss

--- build/index.js
+++ build/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("hello dunia");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+app.route("/").get(function (req, res) {
+    res.send("Hei");
+});
+app.listen(8010);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("hello dunia");
+import express from "express";
+
+const app = express();
+
+app.route("/").get((req, res) => {
+  res.send("Hei");
+});
+
+app.listen(8010);

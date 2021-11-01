43ccba4cf55231aadd990a10216e9c2173164e69
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-11-01 08:47:39 +0700

    video 3 - implementasi ts

--- build/index.js
+++ build/index.js
@@ -3,9 +3,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// Ambil express dan interfacenya
 var express_1 = __importDefault(require("express"));
-var app = (0, express_1.default)();
-app.route("/").get(function (req, res) {
-    res.send("Hei");
-});
-app.listen(8010);
+var App = /** @class */ (function () {
+    function App() {
+        this.app = (0, express_1.default)();
+    }
+    App.prototype.routes = function () {
+        this.app.route("/").get(function (req, res) {
+            res.send("Ini routes route menggnakan ts");
+        });
+    };
+    return App;
+}());
+var port = 8000;
+var app = new App().app;
+app.listen(port, function () { return console.log("Berjalan di PORT " + port); });
+// const app = express();
+// app.route("/").get((req, res) => {
+//   res.send("Hei");
+// });
+// app.listen(8010);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
-import express from "express";
+// Ambil express dan interfacenya
+import express, { Application, Request, Response } from "express";
 
-const app = express();
+class App {
+  public app: Application;
 
-app.route("/").get((req, res) => {
-  res.send("Hei");
-});
+  constructor() {
+    this.app = express();
+  }
 
-app.listen(8010);
+  protected routes(): void {
+    this.app.route("/").get((req: Request, res: Response) => {
+      res.send("Ini routes route menggnakan ts");
+    });
+  }
+}
+
+const port: number = 8000;
+const app = new App().app;
+app.listen(port, () => console.log(`Berjalan di PORT ${port}`));
+
+// const app = express();
+
+// app.route("/").get((req, res) => {
+//   res.send("Hei");
+// });
+
+// app.listen(8010);

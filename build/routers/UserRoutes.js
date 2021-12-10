226dc98325336ccfe6f5797c1738f4e896413bc2
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-12-10 13:36:10 +0700

    selesai video 10 - refactor

--- build/routers/UserRoutes.js
+++ build/routers/UserRoutes.js
@@ -1,15 +1,30 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
+var BaseRouter_1 = __importDefault(require("./BaseRouter"));
 // Controller
 var UserController_1 = __importDefault(require("../controllers/UserController"));
-var UserRoutes = /** @class */ (function () {
+var UserRoutes = /** @class */ (function (_super) {
+    __extends(UserRoutes, _super);
     function UserRoutes() {
-        this.router = (0, express_1.Router)();
-        this.routes();
+        return _super !== null && _super.apply(this, arguments) || this;
     }
     UserRoutes.prototype.routes = function () {
         this.router.get("/", UserController_1.default.index);
@@ -19,5 +34,5 @@ var UserRoutes = /** @class */ (function () {
         this.router.delete("/:id", UserController_1.default.delete);
     };
     return UserRoutes;
-}());
+}(BaseRouter_1.default));
 exports.default = new UserRoutes().router;

7257384e29042a5c752d33f8636546547ba69aef
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-12-10 16:10:41 +0700

    selesai video 16 - validasi request

--- build/routers/AuthRouters.js
+++ build/routers/AuthRouters.js
@@ -4,11 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const BaseRouter_1 = __importDefault(require("./BaseRouter"));
+const AuthValidator_1 = __importDefault(require("../middlewares/AuthValidator"));
 // Controller
 const AuthController_1 = __importDefault(require("../controllers/AuthController"));
 class AuthRoutes extends BaseRouter_1.default {
     routes() {
-        this.router.post("/register", AuthController_1.default.register);
+        this.router.post("/register", AuthValidator_1.default, AuthController_1.default.register);
         this.router.post("/login", AuthController_1.default.login);
     }
 }
--- src/routers/AuthRouters.ts
+++ src/routers/AuthRouters.ts
@@ -1,11 +1,12 @@
 import BaseRoutes from "./BaseRouter";
+import validate from "../middlewares/AuthValidator";
 
 // Controller
 import AuthController from "../controllers/AuthController";
 
 class AuthRoutes extends BaseRoutes {
   public routes(): void {
-    this.router.post("/register", AuthController.register);
+    this.router.post("/register", validate, AuthController.register);
     this.router.post("/login", AuthController.login);
   }
 }

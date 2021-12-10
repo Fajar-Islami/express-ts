376b1bfc87e4bbc0e9ad542d58c72ea643c68b45
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-12-10 16:02:04 +0700

    selesai video 15 - bcrypt

--- build/controllers/AuthController.js
+++ build/controllers/AuthController.js
@@ -8,15 +8,20 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const PasswordHash_1 = __importDefault(require("../utils/PasswordHash"));
 const db = require("../db/models"); // gk semua harus pke ts
 class AuthController {
     constructor() {
         this.register = (req, res) => __awaiter(this, void 0, void 0, function* () {
             let { username, password } = req.body;
+            const hashedPassword = yield PasswordHash_1.default.hash(password);
             yield db.user.create({
                 username,
-                password,
+                password: hashedPassword,
             });
             return res.status(201).send("Registrasi sukses");
         });
--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express";
+import PasswordHash from "../utils/PasswordHash";
 
 const db = require("../db/models"); // gk semua harus pke ts
 
 class AuthController {
   register = async (req: Request, res: Response): Promise<Response> => {
     let { username, password } = req.body;
+    const hashedPassword: string = await PasswordHash.hash(password);
+
     await db.user.create({
       username,
-      password,
+      password: hashedPassword,
     });
 
     return res.status(201).send("Registrasi sukses");
